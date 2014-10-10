9919d260db0ea3d5bf0fe98032efeedc9fb0eb77
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-10-09 22:27:30 -0500

    yes

--- app/views/index.php
+++ app/views/index.php
@@ -28,38 +28,39 @@
         </li>
     </ol>
 
+
+    <form method='POST' action='index.php'>
+        <p class="radioHeaders"> How many paragraphs would you like? (Maximum: 99)</p>
+        <input type="text" name="numberOfParagraphs"><br>
+        <input type='submit' value='Generate Text' id="generateTextButton"><br>
+    </form>
+
     <h2>
         Random User Generator:
     </h2>
     <ol>
         <li>
-            Select how many words you would like in your password (maximum size is 9 words)
+            Select how many paragraphs you would like to be generated (the maximum allowed is 99 paragraphs).
         </li>
         <li>
-            Select if you would like to include a symbol
-        </li>
-        <li>
-            Click "Submit" and you will receive your password!
+            Click "Generate Text" and you will receive your text.
         </li>
     </ol>
 
-    <form method='POST' action='index.php'>
-        <p class="radioHeaders"> How many paragraphs would you like? </p>
-        <input type='generateText' value='Generate Text' id="generateText"><br>
-        <input type="text" name="numberOfWords"><br>
-        <input type="checkbox" name="symbol" value="true" id="symbol"><label for="symbol">Would you like to include a symbol in your password?</label><br>
-        <input type="checkbox" name="number" value="true" id="number"><label for="number">Would you like to include a number in your password?</label><br>
-        <p class="radioHeaders"> Choose one option for your spacing: </p>
-        <input type="radio" name="separatorOptions" value="noSeparator" id="noSeparator" checked><label for="noSeparator">No Separators</label><br/>
-        <input type="radio" name="separatorOptions" value="spaces" id="spaces"><label for="spaces">Spaces</label><br/>
-        <input type="radio" name="separatorOptions" value="underscores" id="underscores"><label for="underscores">Underscores</label><br/>
-        <input type="radio" name="separatorOptions" value="hyphens" id="hyphens"><label for="hyphens">Hyphens</label>
-        <p class="radioHeaders"> Choose one option for your capitalization: </p>
-        <input type="radio" name="capOptions" value="initialCaps" checked id="initialCaps"><label for="initialCaps">Initial Caps</label><br/>
-        <input type="radio" name="capOptions" value="allLowercase" id="allLowercase"><label for="allLowercase">Lower Case</label><br/>
-        <input type="radio" name="capOptions" value="allUppercase" id="allUppercase"><label for="allUppercase">Upper Case</label><br/>
-        <p class="radioHeaders">Click to receive your new password: </p>
-        <input type='submit' value='Submit' id="submit"><br>
+    <input type="text" name="numberOfWords"><br>
+    <input type="checkbox" name="symbol" value="true" id="symbol"><label for="symbol">Would you like to include a symbol in your password?</label><br>
+    <input type="checkbox" name="number" value="true" id="number"><label for="number">Would you like to include a number in your password?</label><br>
+    <p class="radioHeaders"> Choose one option for your spacing: </p>
+    <input type="radio" name="separatorOptions" value="noSeparator" id="noSeparator" checked><label for="noSeparator">No Separators</label><br/>
+    <input type="radio" name="separatorOptions" value="spaces" id="spaces"><label for="spaces">Spaces</label><br/>
+    <input type="radio" name="separatorOptions" value="underscores" id="underscores"><label for="underscores">Underscores</label><br/>
+    <input type="radio" name="separatorOptions" value="hyphens" id="hyphens"><label for="hyphens">Hyphens</label>
+    <p class="radioHeaders"> Choose one option for your capitalization: </p>
+    <input type="radio" name="capOptions" value="initialCaps" checked id="initialCaps"><label for="initialCaps">Initial Caps</label><br/>
+    <input type="radio" name="capOptions" value="allLowercase" id="allLowercase"><label for="allLowercase">Lower Case</label><br/>
+    <input type="radio" name="capOptions" value="allUppercase" id="allUppercase"><label for="allUppercase">Upper Case</label><br/>
+    <p class="radioHeaders">Click to receive your new password: </p>
+    <input type='submit' value='Submit' id="submit"><br>
     </form>
 
     <p>Here is your new password:</p>
