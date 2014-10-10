ba8f81de08511dd8ae7211d0514ba38f2c0cc67b
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-10-10 00:38:31 -0500

    dhg

--- app/views/hello.php
+++ app/views/hello.php
@@ -6,7 +6,6 @@
     <link href='http://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Varela+Round' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/index.css" type="text/css">
-
 </head>
 <body>
 <h1>
