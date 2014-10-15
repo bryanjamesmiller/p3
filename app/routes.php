8bfaf502ffe6a7309decf6afd50f965a5a9af921
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-10-14 21:39:45 -0500

    good stuff blade

--- app/routes.php
+++ app/routes.php
@@ -19,32 +19,34 @@ Route::get('/', function()
 // Display the form for generating users
 Route::get('/users', function() {
 
-    echo "users";
+    return View::make('users');
 
 });
 
 // Process form for generating users
 Route::post('/users', function() {
 
-    echo "users";
+    echo "users post";
 
 });
 
 // Display the form for generating paragraphs
 Route::get('/paragraphs', function() {
 
-    echo "paragraphs";
+    return View::make('paragraphs');
 
 });
 
 // Process form for generating paragraphs
 Route::post('/paragraphs', function() {
 
-    echo "paragraphs";
+    echo "paragraphs post";
 
 });
 
 // Tells you the environment you are on
 Route::get('/practice', function() {
+
     echo App::environment();
+
 });
---
+++ app/views/paragraphs.blade.php
@@ -0,0 +1,9 @@
+@extends('_myBladeTemplate')
+
+@section('title')
+List all the books
+@stop
+
+@section('content')
+<h1>Placeholder Paragraph Text Generator</h1>
+@stop
