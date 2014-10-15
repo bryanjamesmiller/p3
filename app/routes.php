7637ab4c7ace6866d8a5b48a85a3593405d836a5
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-10-14 21:28:31 -0500

    progress

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,36 @@ Route::get('/', function()
 {
 	return View::make('index');
 });
+
+// Display the form for generating users
+Route::get('/users', function() {
+
+    echo "users";
+
+});
+
+// Process form for generating users
+Route::post('/users', function() {
+
+    echo "users";
+
+});
+
+// Display the form for generating paragraphs
+Route::get('/paragraphs', function() {
+
+    echo "paragraphs";
+
+});
+
+// Process form for generating paragraphs
+Route::post('/paragraphs', function() {
+
+    echo "paragraphs";
+
+});
+
+// Tells you the environment you are on
+Route::get('/practice', function() {
+    echo App::environment();
+});
