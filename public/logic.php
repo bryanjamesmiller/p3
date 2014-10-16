6f4aad4a60aff4292d163752c558e57224030c90
Bryan Miller <bryanjamesmiller@fas.harvard.edu>
2014-10-16 13:03:26 -0500

    good progress

--- public/logic.php
+++ public/logic.php
@@ -14,39 +14,45 @@ $users='';
 
 $randomGenerator = Faker\Factory::create();
 
-if($_POST['numberOfParagraphs'] <= $maxSize && $_POST['numberOfParagraphs'] > $minSize)
+if(isset($_POST['numberOfParagraphs']))
 {
-    $paragraphs .= "<p class='middle'>Here are your paragraphs:</p>";
-    for($i = 0; $i < $_POST['numberOfParagraphs']; $i++)
+    if($_POST['numberOfParagraphs'] <= $maxSize && $_POST['numberOfParagraphs'] > $minSize)
     {
-        $paragraphs .= "<p class='output'>" . $randomGenerator->text . "</p>";
+        $paragraphs .= "<p class='middle'>Here are your paragraphs:</p>";
+        for($i = 0; $i < $_POST['numberOfParagraphs']; $i++)
+        {
+            $paragraphs .= "<p class='output'>" . $randomGenerator->text . "</p>";
+        }
     }
-}
-else
-{
-    $paragraphs = "<p>Enter a # of paragraphs from 1 to 99 above<p>";
+    else
+    {
+        $paragraphs = "<p>Enter a # of paragraphs from 1 to 99 above<p>";
+    }
 }
 
-if($_POST['numberOfUsers'] <= $maxSize && $_POST['numberOfUsers'] > $minSize)
+if(isset($_POST['numberOfUsers']))
 {
-    $users .= "<p class='middle'>Here are your Users:</p>";
-    for($i = 0; $i < $_POST['numberOfUsers']; $i++)
+    if($_POST['numberOfUsers'] <= $maxSize && $_POST['numberOfUsers'] > $minSize)
     {
-        $users .= "<p class='output'>" . $randomGenerator->name . "<br>";
-        if($_POST['email'] == 'true')
+        $users .= "<p class='middle'>Here are your Users:</p>";
+        for($i = 0; $i < $_POST['numberOfUsers']; $i++)
         {
-           $users .= $randomGenerator->email . "<br>";
+            $users .= "<p class='output'>" . $randomGenerator->name . "<br>";
+            if($_POST['email'] == 'true')
+            {
+                $users .= $randomGenerator->email . "<br>";
+            }
+            if($_POST['address'] == 'true')
+            {
+                $users .= $randomGenerator->address;
+            }
+            $users .= "</p>";
         }
-        if($_POST['address'] == 'true')
-        {
-            $users .= $randomGenerator->address;
-        }
-        $users .= "</p>";
     }
-}
-else
-{
-    $users = "<p>Please only enter a # of Users from 1 to 99 above<p>";
+    else
+    {
+        $users = "<p>Please only enter a # of Users from 1 to 99 above<p>";
+    }
 }
 
 ?>
